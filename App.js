aedad7ed8d48adb9b844f17178879c1e040e3bea
chazzox <42206116+chazzox@users.noreply.github.com>
2019-11-07 12:09:53 +0000

    the gaming has started, it's time to get that bread

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class bigre extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={{ width: 40, height: 40 }} source={require('.assets/img/navbar.png')} />
+        <Text style={styles.Text}>
+          Ketan
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    backgroundColor: '#222', 
+    width: "100%"
+  },
+  Text:{
+    paddingTop: 25,
+    paddingBottom:10,
+    paddingLeft: 15,
+    alignSelf: 'flex-start',
+    color:"white",
+    fontSize: 25
+  }
+})
