f06eec996d48dfe6b151b6bcbebcc979e7f8fe88
charlie <42206116+chazzox@users.noreply.github.com>
2020-01-29 09:10:06 +0000

    adding props to homework

--- components/screens/homework/homework.js
+++ components/screens/homework/homework.js
@@ -1,111 +1,78 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Dimensions, ScrollView, Text, View } from 'react-native';
 import { SceneMap } from 'react-native-tab-view';
 import TabName from '../templates/weekViewMain/weekView';
 import styles from '../templates/weekViewMain/weekView.style';
 
+class HWobj extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		let dueText;
+		// conditional rendering statement
+		if (this.props.dateDue) {
+			dueText = this.props.dateDue;
+		}
+		return (
+			<View style={styles.block}>
+				<Text style={styles.blockTextMain}>{this.props.main}</Text>
+				<Text style={styles.blockTextBody}>
+					{this.props.body}
+					<Text
+						style={{
+							color: 'white',
+							fontWeight: 'bold',
+							marginTop: 7
+						}}>
+						{dueText}
+						<Text style={{ color: '#007Aff' }}>
+							{this.props.date}
+						</Text>
+					</Text>
+				</Text>
+			</View>
+		);
+	}
+}
+
 const Monday = () => (
-    <View style={styles.screen}>
-        <ScrollView>
-            <View style={styles.block}>
-                <Text style={styles.blockTextMain}>Maths - header</Text>
-                <Text style={styles.blockTextBody}>
-                    descriptions of the homework, its a test lmao also please do
-                    this that and the other lmaoooooooooooo
-                    {'\n'}
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            marginTop: 7
-                        }}
-                    >
-                        Due:
-                        <Text style={{ color: '#007Aff' }}>12/07/20</Text>
-                    </Text>
-                </Text>
-            </View>
-            <View style={styles.block}>
-                <Text style={styles.blockTextMain}>Maths - header</Text>
-                <Text style={styles.blockTextBody}>
-                    descriptions of the homework, its a test lmao also please do
-                    this that and the other lmaoooooooooooo
-                    {'\n'}
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            marginTop: 7
-                        }}
-                    >
-                        Due:
-                        <Text style={{ color: '#007Aff' }}>12/07/20</Text>
-                    </Text>
-                </Text>
-            </View>
-            <View style={styles.block}>
-                <Text style={styles.blockTextMain}>Maths - header</Text>
-                <Text style={styles.blockTextBody}>
-                    descriptions of the homework, its a test lmao also please do
-                    this that and the other lmaoooooooooooo
-                    {'\n'}
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            marginTop: 7
-                        }}
-                    >
-                        Due:
-                        <Text style={{ color: '#007Aff' }}>12/07/20</Text>
-                    </Text>
-                </Text>
-            </View>
-            <View style={styles.block}>
-                <Text style={styles.blockTextMain}>Maths - header</Text>
-                <Text style={styles.blockTextBody}>
-                    descriptions of the homework, its a test lmao also please do
-                    this that and the other lmaoooooooooooo
-                    {'\n'}
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            marginTop: 7
-                        }}
-                    >
-                        Due:
-                        <Text style={{ color: '#007Aff' }}>12/07/20</Text>
-                    </Text>
-                </Text>
-            </View>
-        </ScrollView>
-    </View>
+	<View style={styles.screen}>
+		<ScrollView>
+			<HWobj
+				main='Maths - header'
+				body={`descriptions of the homework, its a test lmao also please do this that and the other lmaoooooooooooo${'\n'}`}
+				date='12/07/20'
+			/>
+			<HWobj />
+		</ScrollView>
+	</View>
 );
 
 const screen = () => (
-    <View style={styles.screen}>
-        <ScrollView>
-            <View style={styles.block}></View>
-            <View style={styles.block}></View>
-            <View style={styles.block}></View>
-            <View style={styles.block}></View>
-        </ScrollView>
-    </View>
+	<View style={styles.screen}>
+		<ScrollView>
+			<HWobj />
+			<HWobj />
+			<HWobj />
+			<HWobj />
+		</ScrollView>
+	</View>
 );
 
-export default class homeworkMain extends React.Component {
-    render() {
-        return (
-            <TabName
-                screenBoi={SceneMap({
-                    Monday: Monday,
-                    Tuesday: Monday,
-                    Wednesday: screen,
-                    Thursday: screen,
-                    Friday: screen
-                })}
-            />
-        );
-    }
+export default class homeworkMain extends Component {
+	render() {
+		return (
+			<TabName
+				style={{ height: 90 }}
+				screenBoi={SceneMap({
+					Monday: Monday,
+					Tuesday: Monday,
+					Wednesday: screen,
+					Thursday: screen,
+					Friday: screen
+				})}
+			/>
+		);
+	}
 }
