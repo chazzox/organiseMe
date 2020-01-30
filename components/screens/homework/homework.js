18606633c95b1d4ef7cecd587c923169edba7832
charlie <42206116+chazzox@users.noreply.github.com>
2020-01-30 22:41:18 +0000

    starting to automate homework view loading using a json file
    prooving to be tricker then it first seems

--- components/screens/homework/homework.js
+++ components/screens/homework/homework.js
@@ -4,6 +4,8 @@ import { SceneMap } from 'react-native-tab-view';
 import TabName from '../templates/weekViewMain/weekView';
 import styles from '../templates/weekViewMain/weekView.style';
 
+import { users } from '../user';
+
 class HWobj extends Component {
 	constructor(props) {
 		super(props);
@@ -13,6 +15,9 @@ class HWobj extends Component {
 		// conditional rendering statement
 		if (this.props.dateDue) {
 			dueText = this.props.dateDue;
+		}
+		if (this.props) {
+			blockTextBody = 'No homework for this day';
 		}
 		return (
 			<View style={styles.block}>
@@ -26,51 +31,86 @@ class HWobj extends Component {
 							marginTop: 7
 						}}>
 						{dueText}
-						<Text style={{ color: '#007Aff' }}>
-							{this.props.date}
-						</Text>
+						<Text style={{ color: '#007Aff' }}>{this.props.date}</Text>
 					</Text>
 				</Text>
 			</View>
 		);
 	}
 }
-
-const Monday = () => (
-	<View style={styles.screen}>
-		<ScrollView>
-			<HWobj
-				main='Maths - header'
-				body={`descriptions of the homework, its a test lmao also please do this that and the other lmaoooooooooooo${'\n'}`}
-				date='12/07/20'
-			/>
-			<HWobj />
-		</ScrollView>
-	</View>
-);
-
-const screen = () => (
-	<View style={styles.screen}>
-		<ScrollView>
-			<HWobj />
-			<HWobj />
-			<HWobj />
-			<HWobj />
-		</ScrollView>
-	</View>
-);
+class Dayobj extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<View style={styles.screen}>
+				<ScrollView>{...this.props}</ScrollView>
+			</View>
+		);
+	}
+}
 
 export default class homeworkMain extends Component {
+	constructor() {
+		this.createSchedule = this.createSchedule.bind(this);
+		this.RenderSchedule = this.RenderSchedule.bind(this);
+	}
+	createSchedule() {
+		let homeworkArr = [[], [], [], [], [], []];
+		for (let hwIterator = 0; hwIterator < users.homework.length; hwIterator++) {
+			if (
+				new Date(
+					new Date().getFullYear(),
+					new Date().getMonth(),
+					new Date().getDate() + 7
+				).getTime() > users.homework[hwIterator].due &&
+				new Date(
+					new Date().getFullYear(),
+					new Date().getMonth(),
+					new Date().getDate()
+				).getTime() <= users.homework[hwIterator].due
+			) {
+				honestlyIHaveNoIdea = Math.floor(
+					new Date(
+						new Date().getFullYear(),
+						new Date().getMonth(),
+						new Date().getDate() + 7
+					).getTime() -
+						new Date(users.homework[hwIterator].due - 1).getTime() / 86400000
+				);
+				console.log(honestlyIHaveNoIdea);
+				homeworkArr[honestlyIHaveNoIdea].push(users.homework[hwIterator]);
+			}
+			homeworkArr[new Date(users.homework[hwIterator].due - 1).getDay()].sort(function(a, b) {
+				return a.subjectId - b.subjectId;
+			});
+		}
+
+		return homeworkArr;
+	}
+	RenderSchedule() {
+		weeksHomework = this.createSchedule();
+		let dayRender = [];
+		for (var i = 0; i < weeksHomework.length; i++) {
+			ppppppp;
+		}
+		return [{}, {}, {}, {}, {}];
+	}
+	componentDidMount() {
+		screenArr = RenderSchedule();
+	}
+
 	render() {
 		return (
 			<TabName
 				style={{ height: 90 }}
 				screenBoi={SceneMap({
-					Monday: Monday,
-					Tuesday: Monday,
-					Wednesday: screen,
-					Thursday: screen,
-					Friday: screen
+					Monday: screenArr[0],
+					Tuesday: screenArr[1],
+					Wednesday: screenArr[2],
+					Thursday: screenArr[3],
+					Friday: screenArr[5]
 				})}
 			/>
 		);
